a65203845a01d87a3c3d9783656d24f4efcf69b4
Kohei Yamamoto <kymmt90@gmail.com>
2015-09-10 21:54:10 +0900

    Add the access to hatenablog's categories list

--- blog.rb
+++ blog.rb
@@ -1,24 +1,33 @@
 #!/usr/bin/env ruby
 # coding: utf-8
 
+# get hatenablog information
+
 require 'net/https'
-
-# Get the first page of the blog entries list.
-# Unsafe because it's using Basic authentication.
-
-Net::HTTP.version_1_2
 
 USER_ID = ARGV[0]
 BLOG_ID = ARGV[1]
 API_KEY = ARGV[2]
 
-req = Net::HTTP::Get.new("/#{USER_ID}/#{BLOG_ID}/atom/entry");
-req.basic_auth USER_ID, API_KEY
+def basic_auth_request(uri)
+  base_uri = "/#{USER_ID}/#{BLOG_ID}/atom/"
+  request = Net::HTTP::Get.new(base_uri + uri)
+  request.basic_auth USER_ID, API_KEY
+  request
+end
 
-https = Net::HTTP.new('blog.hatena.ne.jp', 443)
-https.use_ssl = true
-https.verify_mode = OpenSSL::SSL::VERIFY_NONE
-https.start do
-  response = https.request(req)
-  puts response.body
+hatenablog = Net::HTTP.new('blog.hatena.ne.jp', 443)
+hatenablog.use_ssl = true
+hatenablog.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+hatenablog.start do
+  # display the first page of entries list
+  request = basic_auth_request('entry')
+  puts 'entries list'
+  puts hatenablog.request(request).body
+
+  # display the categories list
+  request = basic_auth_request('category')
+  puts 'categories list'
+  puts hatenablog.request(request).body
 end
